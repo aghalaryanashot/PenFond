fb4ced641f88b460abb27d0e94af92677b6dfc13
Ophir LOJKINE <o.lojkine@qwant.com>
2018-08-02 14:35:21 +0200

    Add query beans

--- db-create-all.sql
+++ db-create-all.sql
@@ -6,8 +6,8 @@ create table company (
 
 create table job_info (
   id                            bigserial not null,
-  id_person                     bigint not null,
-  id_company                    bigint not null,
+  person_id                     bigint,
+  company_id                    bigint,
   base_rate                     integer not null,
   date_arrival                  timestamptz,
   date_exit                     timestamptz,
@@ -21,7 +21,13 @@ create table person (
   patronymic                    varchar(255),
   inn                           bigint not null,
   snils                         bigint not null,
-  date_of_birth                 timestamptz not null,
+  date_of_birth                 timestamptz,
   constraint pk_person primary key (id)
 );
+
+alter table job_info add constraint fk_job_info_person_id foreign key (person_id) references person (id) on delete restrict on update restrict;
+create index ix_job_info_person_id on job_info (person_id);
+
+alter table job_info add constraint fk_job_info_company_id foreign key (company_id) references company (id) on delete restrict on update restrict;
+create index ix_job_info_company_id on job_info (company_id);
 
